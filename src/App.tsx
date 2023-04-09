47bcd94e5d15cc8e79151755d9c2335be3c71e8f
Gavin King <gavin.king@cgi.com>
2023-04-09 08:37:20 +0200

    App basic state before experiment with rtk query

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useAppDispatch, useAppSelector } from './redux/hooks'
+import { increment } from './redux/slices/counterSlice'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const dispatch = useAppDispatch()
+
+  const count = useAppSelector(({ counter }) => counter.value)
+
+  const handleClick = () => {
+    dispatch(increment())
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>Clicked {count} times</button>
     </div>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Provider } from 'react-redux'
+
+import { store } from './redux/store'
+import { App } from './App'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('No root element found')
+}
+
+ReactDOM
+  .createRoot(rootElement)
+  .render(
+    <React.StrictMode>
+      <Provider store={store}>
+      <App />
+      </Provider>
+    </React.StrictMode>,
+  )
